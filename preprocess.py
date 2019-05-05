7f0d301e4411030a1c1a87bce8486f5b7b553b79
Nancy <13165481690@163.com>
2019-05-05 11:18:25 +0800

    增加beijing和thchs-30

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,7 @@ import argparse
 import os
 from multiprocessing import cpu_count
 from tqdm import tqdm
-from datasets import blizzard, ljspeech
+from datasets import blizzard, ljspeech, beijing, thchs
 from hparams import hparams
 
 
@@ -21,6 +21,19 @@ def preprocess_ljspeech(args):
   metadata = ljspeech.build_from_path(in_dir, out_dir, args.num_workers, tqdm=tqdm)
   write_metadata(metadata, out_dir)
 
+def process_beijing(args):
+  in_dir = os.path.join(args.base_dir, 'Beijing')
+  out_dir = os.path.join(args.base_dir, args.output)
+  os.makedirs(out_dir, exist_ok=True)
+  metadata = beijing.build_from_path(in_dir, out_dir, args.num_workers, tqdm=tqdm)
+  write_metadata(metadata, out_dir)
+
+def preprocess_thchs(args):
+  in_dir = os.path.join(args.base_dir, 'data_thchs30')
+  out_dir = os.path.join(args.base_dir, args.output)
+  os.makedirs(out_dir, exist_ok=True)
+  metadata = thchs.build_from_path(in_dir, out_dir, args.num_workers, tqdm=tqdm)
+  write_metadata(metadata, out_dir)
 
 def write_metadata(metadata, out_dir):
   with open(os.path.join(out_dir, 'train.txt'), 'w', encoding='utf-8') as f:
@@ -37,13 +50,17 @@ def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('--base_dir', default=os.path.expanduser('~/tacotron'))
   parser.add_argument('--output', default='training')
-  parser.add_argument('--dataset', required=True, choices=['blizzard', 'ljspeech'])
+  parser.add_argument('--dataset', required=True, choices=['blizzard', 'ljspeech', 'beijing', 'thchs-30'])
   parser.add_argument('--num_workers', type=int, default=cpu_count())
   args = parser.parse_args()
   if args.dataset == 'blizzard':
     preprocess_blizzard(args)
   elif args.dataset == 'ljspeech':
     preprocess_ljspeech(args)
+  elif args.dataset == 'beijing':
+    process_beijing(args)
+  elif args.dataset == 'thchs-30':
+    preprocess_thchs(args)
 
 
 if __name__ == "__main__":
